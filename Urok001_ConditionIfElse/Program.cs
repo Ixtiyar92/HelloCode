9dd7176eb72bc3600858c23ea8d446d2a952f128
Aliyev Ixtiyar <amfibiya-man@mail.ru>
2023-02-08 00:53:40 +0700

    add ConditionIfElse

---
+++ Urok001_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "Маша")
+{
+     Сonsole.WriteLine("Ура, это же МАША");
+}
+else
+{
+     Console.Write("Привет, ");
+     Сonsole.WriteLine(username);
+}
