b156df213eee76f23c1032be7ba13d4ae7b51c78
Aliyev Ixtiyar <amfibiya-man@mail.ru>
2023-02-14 02:24:56 +0700

    Решил задачу 2

---
+++ Seminar001/2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2){
+    Console.WriteLine($"Большее число: {number1}, меньшее число: {number2}");
+    }
+else
+{
+    Console.WriteLine($"Большее число: {number2}, меньшее число: {number1}");
+}
--- Seminar001/5/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-int i = -1 * number;
-while (i <= number){
-    Console.Write($"{i} ");
-    i++;
-}
