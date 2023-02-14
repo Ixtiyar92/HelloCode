874f2e70574db8a435322a8c2a4dbc9ab6adacde
Aliyev Ixtiyar <amfibiya-man@mail.ru>
2023-02-15 03:41:18 +0700

    Решил задачу 8

---
+++ Seminar001/8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i < N; i++){
+    if (i % 2 == 0){
+       Console.Write($"{i} "); 
+    }
+}    
