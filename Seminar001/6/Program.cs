faf00eac6c1a5fe4e2ff3a58d0946e40dd1c9f22
Aliyev Ixtiyar <amfibiya-man@mail.ru>
2023-02-15 02:37:21 +0700

    Решил задачу 6

---
+++ Seminar001/6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0){
+    Console.WriteLine($"Число {number} чётное");
+}
+else{
+    Console.WriteLine($"Число {number} нечётное");
+}
