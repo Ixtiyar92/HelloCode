615c9cb558f09bbac3f82937109d575faaad5984
AliyevIxtiyar <amfibiya-man@mail.ru>
2023-02-27 02:03:32 +0700

    Лекция 3

---
+++ Lekciya003/3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Упорядочивание данных внутри массива
+// Было: 6 8 3 2 1 4 5 7
+// Стало: 8 7 6 5 4 3 2 1
+
+// Как это сделать?
+
+// 1. Найти позицию максимального элемента в неотсортированной части массива.
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции.
+// 3. Повторять пока есть неотсортированные элементы.
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 8, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+SelectionSort(arr);
+PrintArray(arr);
