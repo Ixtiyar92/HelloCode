06f7f37c577008d66638114a548aa1b5820f45ef
AliyevIxtiyar <amfibiya-man@mail.ru>
2023-02-27 02:38:56 +0700

    Изменили решение задачи 19

--- Seminar003/19/Program.cs
+++ Seminar003/19/Program.cs
@@ -4,6 +4,7 @@
 // 12821 -> да
 // 23432 -> да
 
+/*
 Console.Clear();
 Console.Write("Введите пятизначное число: ");
 int num = int.Parse(Console.ReadLine());
@@ -14,4 +15,20 @@ if (i == j){
 Console.WriteLine("Это число является палиндромом");
 }else{
 Console.WriteLine("Это число не является палиндромом");
+}
+*/
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5){
+    if (number[0] == number[4] && number[1] == number[3]){
+        Console.WriteLine("Это число является палиндромом");
+    }else{
+        Console.WriteLine("Это число не является палиндромом");
+    }
+}else{
+    Console.WriteLine($"ОШИБКА: {number} - число не является пятизначным");
 }
