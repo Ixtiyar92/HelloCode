970ccea12371fac702c7fb2388b6f92a42004ac0
AliyevIxtiyar <amfibiya-man@mail.ru>
2023-02-27 22:14:05 +0700

    Задачи с семинара 4 (25, 27 и 29)

--- Seminar003/19/Program.cs
+++ Seminar003/19/Program.cs
@@ -32,3 +32,22 @@ if (len == 5){
 }else{
     Console.WriteLine($"ОШИБКА: {number} - число не является пятизначным");
 }
+
+/* Решение для любых чисел
+Console.Clear();
+Console.Write("Введите число : ");
+int num = int.Parse(Console.ReadLine());int Revers(int num){
+int revers = 0;// 54321
+while (num > 0)
+{ // num = 0 > 0 54320
+revers = revers * 10 + num % 10; // revers = 5432 * 10 + num%10(1%10 = 1) = 54321
+num /= 10; // num / 10(1/10)
+}// 12345 == 54321
+return revers;
+}
+if (num == Revers(num)){
+Console.Write("Да");
+}else{
+Console.Write("Нет");
+}
+*/
---
+++ Seminar004/28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 5 - 120 == 1 * 2 * 3 * 4 * 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение цифр = {GetMultiply(num)}");
+
+long GetMultiply(long n){ // n=3
+    long result = 1;
+    long i = 2;
+    if( n < 0){
+        i = n;
+        n = -1;
+    }
+    for (; i <= n; i++){ // i <= n; 2 <= 3
+        result *= i; // result
+    }
+    return result;
+}
