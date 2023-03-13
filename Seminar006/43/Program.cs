517e3a5b6596a069ed311aae3d97fb9534c6dafd
Ixtiyar <amfibiya-man@mail.ru>
2023-03-14 00:00:31 +0700

    Задачи 53-60

--- Seminar006/43/Program.cs
+++ Seminar006/43/Program.cs
@@ -3,7 +3,7 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.Clear();
+
 Console.Write("Введите B1: ");
 double b1 = double.Parse(Console.ReadLine());
 Console.Write("Введите K1: ");
